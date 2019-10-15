b2881e3727315b4b0803420ec259756247cdd160
aachour1 <46736922+aachour1@users.noreply.github.com>
2019-10-15 18:20:16 -0400

    Retrieve MAC instead of IP

--- NetworkScanner.py
+++ NetworkScanner.py
@@ -13,16 +13,26 @@ part_of_MAC=False
 #I have it down as a list and it includes the periods, but its
 #p easy to change it to just being an interger without the periods
 next_MAC=""
+counter = 0	
+start = False
 #iterates over entire arp -a results
 for c in com_line_results:
-    if(c==')'):
+    if(c=='['):
         part_of_MAC=False
+        #remove the space after the MAC address 
+        next_MAC = next_MAC[:-1]
         list_mac_addresses.append(next_MAC)
         next_MAC=""
     if(part_of_MAC):
         next_MAC=next_MAC+c
-    if(c=='('):
+    if(c==')'):
+	start = True
+        counter=1
+    if(counter==5 and start==True):
+	start=False
         part_of_MAC=True
+        counter=0;
+    counter += 1
 #prints list of mac addresses
 print(list_mac_addresses)
 file.close()
