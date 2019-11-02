b69c9e14ba945eed35ba7556e2cc3d8643ada0ea
vdiago98 <vincentdiago98@gmail.com>
2019-11-02 12:13:47 -0400

    Added regex to read mac addresses from file

--- NetworkScanner.py
+++ NetworkScanner.py
@@ -1,38 +1,22 @@
 import os
+import re
 #Be connected to your home network
 #Run command line arp -a
 os.system("arp -a > MACaddress.txt")
 #Parse all the MAC addresses into a list
-file=open("MACaddress.txt","r")
-#read entire file of arp -a results into a string
-com_line_results=file.read(-1)
+file=open('MACaddress.txt')
+
 #list of all the MAC addresses of all the devices on the network
 list_mac_addresses=[]
-#boolean if current char is part of a MAC address
-part_of_MAC=False
-#I have it down as a list and it includes the periods, but its
-#p easy to change it to just being an interger without the periods
-next_MAC=""
-counter = 0	
-start = False
+#regex to extract mac addresses from the file
+mac_reg = re.compile("([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})")
 #iterates over entire arp -a results
-for c in com_line_results:
-    if(c=='['):
-        part_of_MAC=False
-        #remove the space after the MAC address 
-        next_MAC = next_MAC[:-1]
-        list_mac_addresses.append(next_MAC)
-        next_MAC=""
-    if(part_of_MAC):
-        next_MAC=next_MAC+c
-    if(c==')'):
-	start = True
-        counter=1
-    if(counter==5 and start==True):
-	start=False
-        part_of_MAC=True
-        counter=0;
-    counter += 1
+for line in file:
+    macAddress = re.search(mac_reg, line)
+    if macAddress:
+        macAddress = macAddress.group()
+        list_mac_addresses.append(macAddress)
 #prints list of mac addresses
 print(list_mac_addresses)
+
 file.close()
